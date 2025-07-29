1546f63548f3189f31da5cbfe1b188b300ad75b6
adimerpaul <adimer101@gmail.com>
2025-07-29 05:19:32 -0400

    Creado version

---
+++ front/dist/pwa/assets/use-quasar-B3j39DKS.js
@@ -0,0 +1,1 @@
+import{z as a,b2 as r}from"./index-DmXF2vU7.js";function u(){return a(r)}export{u};
--- front/dist/pwa/assets/use-quasar-ClfMeSBw.js
+++
@@ -1,1 +0,0 @@
-import{z as a,b2 as r}from"./index-BhipJvXX.js";function u(){return a(r)}export{u};
