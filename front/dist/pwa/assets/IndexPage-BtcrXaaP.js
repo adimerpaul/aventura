80c423491e63adfea9a41f44b90638631bb1b39a
adimerpaul <adimer101@gmail.com>
2025-08-11 07:06:18 -0400

    creado version

---
+++ front/dist/pwa/assets/IndexPage-BtcrXaaP.js
@@ -0,0 +1,1 @@
+import{Q as t}from"./QPage-J3mg8W0A.js";import{c as a,o,w as s,b as r}from"./index-CZ4ZY1DO.js";const x={__name:"IndexPage",setup(l){return(n,e)=>(o(),a(t,{class:"flex flex-center"},{default:s(()=>e[0]||(e[0]=[r("img",{alt:"Quasar logo",src:"logo.png",style:{width:"200px",height:"200px"}},null,-1)])),_:1}))}};export{x as default};
---
+++ front/dist/pwa/assets/QSpace-B3ehM8JM.js
@@ -0,0 +1,1 @@
+import{v as a,j as s}from"./index-CZ4ZY1DO.js";const p=a({name:"QSpace",setup(){const e=s("div",{class:"q-space"});return()=>e}});export{p as Q};
